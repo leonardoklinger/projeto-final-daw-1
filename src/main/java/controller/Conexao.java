a3ef4f11cc08cd11a158851964ab8dac57a2aaaa
Leonardo Klinger <60077995+leonardoklinger@users.noreply.github.com>
2021-11-22 19:24:20 -0300

    Update Conexao.java

--- src/main/java/controller/Conexao.java
+++ src/main/java/controller/Conexao.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 public class Conexao {
     public Connection getConexao() {
         String driver = "org.postgresql.Driver";
-        String user = "postgres";
-        String senha = "rushfan2112";
-        String url = "jdbc:postgresql://localhost:5432/postgres";
+        String user = "bfcawrqdxzszny";
+        String senha = "ac932df7169dc873683c57a72bdcfe1a79bb4f4e92a78e5258700dd1c7203aab";
+        String url = "jdbc:postgresql://ec2-34-205-230-1.compute-1.amazonaws.com:5432/d1bvbui62jahec";
         Connection con = null;
         try {
             Class.forName(driver);
